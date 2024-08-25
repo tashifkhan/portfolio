98258ce349b29da3c082e8ceaaa396726c34ce2e
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2024-08-25 13:17:33 +0530

    navigation bar completed

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import Header from "@/components/Header";
+import React from "react";
+import Hero from "@/components/Hero";
+
+export default function Home() {
+	return (
+		<>
+			<Header />
+			<Hero />
+		</>
+	);
+}
