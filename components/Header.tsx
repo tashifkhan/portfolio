8b06933ac686b1b3754de7e5395c07b03fe5b895
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2024-11-28 22:58:14 +0530

    fixed minor ui inconsitences

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,12 +9,12 @@ const Header: React.FC = () => {
 	return (
 		<header className="z-[999] relative">
 			<motion.div
-				className="fixed top-0 left-1/2 h-[4.5rem] w-[110%] rounded-none border shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full bg-gray-950 border-black/40 bg-opacity-40"
+				className="fixed bottom-0 left-1/2 h-[4.5rem] w-[110%] rounded-none border shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full bg-gray-950 border-black/40 bg-opacity-40"
 				initial={{ y: -100, x: "-50%", opacity: 0 }}
 				animate={{ y: 0, x: "-50%", opacity: 1 }}
 			></motion.div>
 
-			<nav className="flex fixed bottom-3 left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0 sm:bottom-[initial]">
+			<nav className="flex fixed bottom-5 left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0 sm:bottom-[initial]">
 				<ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
 					{links.map((link) => (
 						<motion.li
