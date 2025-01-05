591236c1439132f4a1fe95be78a6c62452b2d1f7
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-01-05 07:40:52 +0530

    Add Vercel Analytics integration to the layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({
 			<body
 				className={`${inter.className}  bg-cover bg-center bg-custom-bg h-screen `}
 			>
+				<Analytics />
 				<Header />
 				{children}
 			</body>
