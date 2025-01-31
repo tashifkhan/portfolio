3c221f43dfc62a148b23a43261995f3d1873477e
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-01-31 23:45:17 +0530

    added hero section navigation

--- lib/navbar-data.tsx
+++ lib/navbar-data.tsx
@@ -1,47 +1,46 @@
 import React from "react";
-import { HiHome } from "react-icons/hi"
-import { BsProjector } from "react-icons/bs"
-import { GiSkills } from "react-icons/gi"
-import { LuGraduationCap } from "react-icons/lu"
-import { GoTrophy } from "react-icons/go"
-import { MdOutlineContactMail } from "react-icons/md"
-
+import { HiHome } from "react-icons/hi";
+import { BsProjector } from "react-icons/bs";
+import { GiSkills } from "react-icons/gi";
+import { LuGraduationCap } from "react-icons/lu";
+import { GoTrophy } from "react-icons/go";
+import { MdOutlineContactMail } from "react-icons/md";
 
 interface Link {
-   name: string
-   hash?: string
-   icon?: React.ReactElement 
+	name: string;
+	hash?: string;
+	icon?: React.ReactElement;
 }
 
 export const links: Link[] = [
-  {
-    name: "Home",
-    hash: "/",
-    icon: <HiHome />
-  },
-  {
-    name: "Projects",
-    hash: "/#projects",
-    icon: <BsProjector />
-  },
-  {
-    name: "Skills",
-    hash: "/#skills",
-    icon: <GiSkills />
-  },
-  {
-    name: "Education",
-    hash: "/#education",
-    icon: <LuGraduationCap />
-  },
-  {
-    name: "Recognistions",
-    hash: "/#por",
-    icon: <GoTrophy />
-  },
-  {
-    name: "Contact",
-    hash: "/#contact",
-    icon: <MdOutlineContactMail />
-  }
-]
+	{
+		name: "Home",
+		hash: "/#hero",
+		icon: <HiHome />,
+	},
+	{
+		name: "Projects",
+		hash: "/#projects",
+		icon: <BsProjector />,
+	},
+	{
+		name: "Skills",
+		hash: "/#skills",
+		icon: <GiSkills />,
+	},
+	{
+		name: "Education",
+		hash: "/#education",
+		icon: <LuGraduationCap />,
+	},
+	{
+		name: "Recognistions",
+		hash: "/#por",
+		icon: <GoTrophy />,
+	},
+	{
+		name: "Contact",
+		hash: "/#contact",
+		icon: <MdOutlineContactMail />,
+	},
+];
