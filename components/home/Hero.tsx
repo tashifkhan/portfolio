a306c60b56891c1ce7e9d28e7a6043a1d9de1358
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-06-16 16:11:20 +0530

    deployment fix 1

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -84,7 +84,14 @@ function Hero() {
 
 	const item = {
 		hidden: { y: 20, opacity: 0 },
-		show: { y: 0, opacity: 1, transition: { type: "spring", stiffness: 100 } },
+		show: { 
+			y: 0, 
+			opacity: 1, 
+			transition: { 
+				type: "spring" as const, 
+				stiffness: 100 
+			} 
+		},
 	};
 
 	return (
